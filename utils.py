348e2677a603b0316e446d2ca9b4b6ca964e5125
dbadhe <dbadhepmm@gmail.com>
2025-05-07 13:36:17 -0700

    Lunar lander

--- utils.py
+++ utils.py
@@ -8,3 +8,65 @@ def get_action(q_values, epsilon):
         # Exploitation: Choose the best known action
         action = np.argmax(q_values)
     return action
+
+def compute_loss(experiences, gamma, q_network, target_q_network):
+    """ 
+    Calculates the loss.
+    
+    Args:
+      experiences: (tuple) tuple of ["state", "action", "reward", "next_state", "done"] namedtuples
+      gamma: (float) The discount factor.
+      q_network: (tf.keras.Sequential) Keras model for predicting the q_values
+      target_q_network: (tf.keras.Sequential) Keras model for predicting the targets
+          
+    Returns:
+      loss: (TensorFlow Tensor(shape=(0,), dtype=int32)) the Mean-Squared Error between
+            the y targets and the Q(s,a) values.
+    """
+
+    # Unpack the mini-batch of experience tuples
+    states, actions, rewards, next_states, done_vals = experiences
+    
+    # Compute max Q^(s,a)
+    max_qsa = tf.reduce_max(target_q_network(next_states), axis=-1)
+    
+    # Set y = R if episode terminates, otherwise set y = R + γ max Q^(s,a).
+    ### START CODE HERE ### 
+    y_targets = rewards + (gamma * max_qsa * (1 - done_vals))
+    ### END CODE HERE ###
+    
+    # Get the q_values and reshape to match y_targets
+    q_values = q_network(states)
+    q_values = tf.gather_nd(q_values, tf.stack([tf.range(q_values.shape[0]),
+                                                tf.cast(actions, tf.int32)], axis=1))
+        
+    # Compute the loss
+    ### START CODE HERE ###
+    
+    loss = MSE(y_targets,q_values) 
+    ### END CODE HERE ### 
+    
+    return loss
+
+def agent_learn(experiences, gamma):
+    """
+    Updates the weights of the Q networks.
+    
+    Args:
+      experiences: (tuple) tuple of ["state", "action", "reward", "next_state", "done"] namedtuples
+      gamma: (float) The discount factor.
+    
+    """
+    
+    # Calculate the loss
+    with tf.GradientTape() as tape:
+        loss = compute_loss(experiences, gamma, q_network, target_q_network)
+
+    # Get the gradients of the loss with respect to the weights.
+    gradients = tape.gradient(loss, q_network.trainable_variables)
+    
+    # Update the weights of the q_network.
+    optimizer.apply_gradients(zip(gradients, q_network.trainable_variables))
+
+    # update the weights of target q_network
+    utils.update_target_network(q_network, target_q_network)
